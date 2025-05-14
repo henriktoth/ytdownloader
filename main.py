011ef38fa4a29e5e6ab26206d396638df53a58a5
Tóth Henrik <henriktoth0517@HenrikMac.local>
2025-05-14 23:57:05 +0200

    feat: created base app

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello, World!")
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+
+app = QApplication(sys.argv)
+layout = QVBoxLayout()
+
+# Window setup
+window = QWidget()
+window.setWindowTitle("Youtube Converter")
+window.resize(600,400)
+window.setLayout(layout)
+window.show()
+
+label = QLabel("Youtube Converter")
+label.setStyleSheet("font-size: 20px; font-weight: bold;")
+layout.addWidget(label)
+
+
+
+
+
+sys.exit(app.exec_())
