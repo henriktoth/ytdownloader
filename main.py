d3f536c5e8ac315f747680bb7687ce894b603b4c
Tóth Henrik <henriktoth0517@HenrikMac.local>
2025-05-28 00:10:29 +0200

    feat: created title

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QFrame
+from PyQt5.QtCore import Qt
 
 app = QApplication(sys.argv)
-layout = QVBoxLayout()
 
-# Window setup
+
+# Window
 window = QWidget()
 window.setWindowTitle("Youtube Converter")
 window.resize(600,400)
+
+layout = QVBoxLayout()
+layout.setAlignment(Qt.AlignTop)
+
 window.setLayout(layout)
 window.show()
 
-label = QLabel("Youtube Converter")
+#Title
+label_layout = QHBoxLayout()
+
+label = QLabel("ConvLoad")
 label.setStyleSheet("font-size: 20px; font-weight: bold;")
-layout.addWidget(label)
+label_layout.addWidget(label)
 
+dev_label = QLabel("developer build v0.1")
+dev_label.setStyleSheet("font-size: 12px; color: grey;")
+dev_label.setAlignment(Qt.AlignRight)
+label_layout.addWidget(dev_label)
 
+layout.addLayout(label_layout)
 
+line = QFrame()
+line.setFrameShape(QFrame.HLine)
+line.setFrameShadow(QFrame.Sunken)
+layout.addWidget(line)
 
 
 sys.exit(app.exec_())
